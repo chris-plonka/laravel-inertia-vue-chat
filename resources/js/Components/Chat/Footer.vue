544ef43bcdffd40ee8320a8f1f85aa5eaadc62df
Tapan Sharma <tapansharma69@gmail.com>
2024-08-08 17:26:49 +0545

    seeding fake messages in order

--- resources/js/Components/Chat/Footer.vue
+++ resources/js/Components/Chat/Footer.vue
@@ -24,11 +24,12 @@ const handleEnter = (e) => {
         class="fixed bottom-0 end-0 start-0 items-center border-t border-slate-200/75 bg-white lg:start-80"
     >
         <textarea
+            rows="2"
             v-model="message"
             v-on:keydown.enter.prevent="handleEnter"
             v-on:keydown.shift="shift = true"
             v-on:keyup="shift = false"
-            class="-mx-5 block w-full rounded-lg border-0 px-5 py-4 leading-6 focus:border-indigo-500 focus:ring focus:ring-indigo-500/75"
+            class="w-11/12 rounded-lg border-0 px-5 ml-4 py-4 focus:border-indigo-500 focus:ring focus:ring-indigo-500/75"
             placeholder="Type a new message and hit enter.."
         ></textarea>
     </footer>
